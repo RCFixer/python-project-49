165edcd80e0bef51e279e9fb738d7ef2b1c864d5
roman <roman.bogdanov@lanit-tercom.com>
2024-01-26 22:18:03 +0300

    added calc game and asciinema link

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 [![Actions Status](https://github.com/RCFixer/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/RCFixer/python-project-49/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/5a4dab013a37d9772352/maintainability)](https://codeclimate.com/github/RCFixer/python-project-49/maintainability)
 
+### Brain-Even
 https://asciinema.org/a/0tXKhfFwoXurwifSObwKsv6X6
+
+### Brain-Calc
+https://asciinema.org/a/rVcGy4dzSBZSTPr4RfPlcszLQ
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,50 @@
+from random import randint, shuffle
+
+import prompt
+
+
+def greetings():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
+def game_logic(first_number, second_number, answer, action, name):
+    print(f"Question: {first_number} {action} {second_number}")
+    question = prompt.string("Your answer: ")
+    if int(question) == answer:
+        print("Correct!")
+        return True
+    else:
+        print(f"'{question}' is wrong answer ;(. Correct answer was '{answer}'.\nLet's try again, {name}!")
+        return False
+
+
+def main():
+    name = greetings()
+    print('What is the result of the expression?')
+    actions = ['+', '-', '*']
+    shuffle(actions)
+    result = True
+    for action in actions:
+        first_number = randint(1, 25)
+        second_number = randint(1, 25)
+        match action:
+            case '+':
+                answer = first_number + second_number
+                result = game_logic(first_number, second_number, answer, action, name)
+            case '-':
+                answer = first_number - second_number
+                result = game_logic(first_number, second_number, answer, action, name)
+            case '*':
+                answer = first_number * second_number
+                result = game_logic(first_number, second_number, answer, action, name)
+        if result is False:
+            break
+    if result is True:
+        print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
